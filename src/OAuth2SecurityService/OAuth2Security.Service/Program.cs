73ae50bd61eaf823e1f934eeb252b39940f83ea1
StuartFerguson <stuart_ferguson1@btinternet.com>
2018-10-30 16:04:56 +0000

    Initial Service skeleton

---
+++ src/OAuth2SecurityService/OAuth2Security.Service/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace OAuth2Security.Service
+{
+    public class Program
+    {
+
+        public static void Main(String[] args)
+        {
+            var host = BuildWebHost(args);
+
+            host.Run();
+        }
+
+        public static IWebHost BuildWebHost(String[] args)
+        {
+            Console.Title = "OAuth2 Security Service";
+
+            String environmentName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
+            IConfigurationRoot config = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("hosting.json", optional: false)
+                .AddJsonFile($"hosting.{environmentName}.json", optional: true)
+                .Build();
+
+            IWebHost host = new WebHostBuilder().UseKestrel().UseConfiguration(config)
+                .UseContentRoot(Directory.GetCurrentDirectory()).UseStartup<Startup>().Build();
+
+            return host;
+        }
+    }
+}
