9a2e8520aa006cdfc3c4a7bc9806b238895499c0
StuartFerguson <stuart_ferguson1@btinternet.com>
2018-11-30 19:07:17 +0000

    Added specflow tests

---
+++ OAuth2SecurityService.IntegrationTests.Specflow/Common/GenericSteps.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Ductus.FluentDocker.Builders;
+using Ductus.FluentDocker.Model.Builders;
+using Ductus.FluentDocker.Services;
+using Ductus.FluentDocker.Services.Extensions;
+using MySql.Data.MySqlClient;
+using TechTalk.SpecFlow;
+
+namespace OAuth2SecurityService.IntegrationTests.Specflow.Common
+{
+    [Binding]
+    public class GenericSteps
+    {
+        protected ScenarioContext ScenarioContext;
+        protected IContainerService SecurityServiceContainer;
+        protected INetworkService TestNetwork;
+        protected Int32 SecurityServicePort;
+        
+        protected GenericSteps(ScenarioContext scenarioContext) 
+        {
+            this.ScenarioContext = scenarioContext;
+        }
+        
+        protected void RunSystem(String testFolder)
+        {
+            String securityServiceContainerName = $"securityService{Guid.NewGuid()}";
+            
+            // Build a network
+            this.TestNetwork = new Builder().UseNetwork($"test-network-{Guid.NewGuid()}").Build();
+            
+            // Security Service Container
+            this.SecurityServiceContainer = new Builder()
+                .UseContainer()
+                .WithName(securityServiceContainerName)
+                .WithEnvironment("SeedingType=IntegrationTest", "ASPNETCORE_ENVIRONMENT=IntegrationTest")
+                .UseImage("oauth2securityserviceservice")
+                .ExposePort(5001)
+                .UseNetwork(this.TestNetwork)                    
+                .Mount($"D:\\temp\\docker\\{testFolder}", "/home", MountType.ReadWrite)
+                .WaitForPort("5001/tcp", 30000)
+                .Build()
+                .Start();
+
+            // Cache the ports
+            this.SecurityServicePort = this.SecurityServiceContainer.ToHostExposedEndpoint("5001/tcp").Port;            
+        }
+
+        protected void StopSystem()
+        {
+            if (this.SecurityServiceContainer != null)
+            {
+                this.SecurityServiceContainer.Stop();
+                this.SecurityServiceContainer.Remove(true);
+            }
+
+            if (this.TestNetwork != null)
+            {
+                this.TestNetwork.Stop();
+                this.TestNetwork.Remove(true);
+            }
+        }        
+    }
+}
---
+++ OAuth2SecurityService.Manager/DbContexts/SeedData/IdentityUserSeedData.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using IdentityServer4.Test;
+using Microsoft.AspNetCore.Identity;
+
+namespace OAuth2SecurityService.Manager.DbContexts.SeedData
+{
+    public class IdentityUserSeedData
+    {
+        public static List<IdentityUser> GetIdentityUsers(SeedingType seedingType)
+        {
+            List<IdentityUser> identityUsers = new List<IdentityUser>();
+
+            if (seedingType == SeedingType.IntegrationTest)
+            {
+                identityUsers.AddRange(SeedTestUsers());
+            }
+
+            return identityUsers;
+        }
+
+        public static List<IdentityUser> SeedTestUsers()
+        {
+            PasswordHasher<IdentityUser> passwordHasher = new PasswordHasher<IdentityUser>();
+
+            List<IdentityUser> testUsers = new List<IdentityUser>();
+            var testUser1 = new IdentityUser
+            {
+                UserName = "integrationtest",
+                NormalizedUserName = "integrationtest",
+                Email = "integrationtest@test.co.uk",
+                NormalizedEmail = "integrationtest@test.co.uk",
+                EmailConfirmed = true,
+                Id = "84DC3E90-16B9-441D-B940-37BAA2AC53AF",                                       
+            };
+            testUser1.PasswordHash = passwordHasher.HashPassword(testUser1, "123456");
+            testUsers.Add(testUser1);
+
+            return testUsers;
+        }
+
+    }
+}
